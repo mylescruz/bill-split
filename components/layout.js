09f276af6a1b0e87de02aace4b37b191c9a642f5
Myles <mylesvirgilcruz@gmail.com>
2024-12-25 13:37:05 -0800

    Set up the home page

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,21 @@
+import styles from "@/styles/layout.module.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
+
 const Layout = () => {
     return (
         <>
-            <h1>Bill Splitter</h1>  
+            <h1 className={styles.title}>Bill Splitter</h1>
+            <Container className={`${styles.container} mx-auto w-75`}>
+                <h4 className="text-center mx-4 my-2">Split your bill <br/> evenly or by item</h4>
+                <Row className="w-50 text-center mx-auto">
+                    <Col className="col-6 my-4">
+                        <Button className={styles.button}>Even</Button>
+                    </Col>
+                    <Col className="col-6 my-4">
+                        <Button className={styles.button}>Item</Button>
+                    </Col>
+                </Row>
+            </Container>
         </>
     );
 };
