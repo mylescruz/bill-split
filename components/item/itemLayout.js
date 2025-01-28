0f0820018b532e2ec0cc8f3c47db735256026a86
Myles <mylesvirgilcruz@gmail.com>
2025-01-28 12:00:01 -0800

    Update the title for each page in 'split by item'

--- components/item/itemLayout.js
+++ components/item/itemLayout.js
@@ -4,6 +4,10 @@ import PeopleForm from "./peopleForm";
 import ItemsForm from "./itemsForm";
 
 const ItemLayout = () => {
+    const infoText = "Enter the bill's information";
+    const peopleText = "Enter the people dining";
+    const itemText = "Enter the items ordered";
+    
     const emptyBill = {
         total: "",
         tax: "",
@@ -23,7 +27,7 @@ const ItemLayout = () => {
 
     return (
         <>
-            <h4 className="text-center mx-2 mt-2 mb-4">Enter the bill&#39;s information</h4>
+            <h4 className="text-center mx-2 mt-2 mb-4">{showInfo ? infoText : (showPeople ? peopleText : itemText)}</h4>
             {showInfo && <InfoForm bill={bill} setBill={setBill} emptyBill={emptyBill} setShowInfo={setShowInfo} setShowPeople={setShowPeople} />}
             {showPeople && <PeopleForm bill={bill} setBill={setBill} setShowInfo={setShowInfo} setShowPeople={setShowPeople} setShowItems={setShowItems} />}
             {showItems && <ItemsForm bill={bill} setBill={setBill} emptyBill={emptyBill} people={bill.people} setShowPeople={setShowPeople} setShowItems={setShowItems} setShowInfo={setShowInfo} />}
