6e6be49c35298793f01d63923f94ae8b47846c60
Myles <mylesvcruz@gmail.com>
2026-01-11 00:07:43 -0600

    redesign: infoForm: Add labels to the info form for more user clarity

--- components/item/infoForm.js
+++ components/item/infoForm.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, Form } from "react-bootstrap";
+import { Button, Col, Form } from "react-bootstrap";
 import styles from "@/styles/infoForm.module.css";
 const MAX_SALES_TAX_PERCENTAGE = 0.125;
 
@@ -75,64 +75,74 @@ const InfoForm = ({ bill, emptyBill, setPage }) => {
     <>
       <h4>Enter the bill&#39;s information</h4>
       <Form onSubmit={enterInfo}>
-        <Form.Group className="form-input">
-          <Form.Control
-            id="total"
-            className="h-100 w-75 mx-auto"
-            type="number"
-            min="0.01"
-            step="0.01"
-            placeholder="Total"
-            value={billDetails.total}
-            onChange={handleNumInput}
-            required
-          />
-        </Form.Group>
-        <Form.Group className="form-input">
-          <Form.Control
-            id="tax"
-            className="h-100 w-75 mx-auto"
-            type="number"
-            min="0.01"
-            step="0.01"
-            placeholder="Tax"
-            value={billDetails.tax}
-            onChange={handleNumInput}
-            required
-          />
-        </Form.Group>
-        <Form.Group className="form-input">
-          <Form.Select
-            id="tipPercentage"
-            className="h-100 w-75 mx-auto"
-            placeholder="Tip"
-            value={billDetails.tipPercentage}
-            onChange={handleInput}
-            required
-          >
-            <option disabled>Tip Percentage</option>
-            <option value="0.15">15%</option>
-            <option value="0.18">18%</option>
-            <option value="0.20">20%</option>
-            <option value="Custom">Custom/Gratuity</option>
-          </Form.Select>
-        </Form.Group>
-        {customTip && (
-          <Form.Group className="form-input">
+        <Col className="my-3">
+          <Form.Group controlId="total">
+            <Form.Label>What was the bill&#39;s total cost?</Form.Label>
             <Form.Control
-              id="customTip"
-              className="h-100 w-75 mx-auto"
+              className="h-100"
               type="number"
               min="0.01"
               step="0.01"
-              placeholder="Tip/Gratuity ($)"
-              value={billDetails.tip}
-              onChange={handleCustomTip}
+              placeholder="Total"
+              value={billDetails.total}
+              onChange={handleNumInput}
               required
             />
           </Form.Group>
+        </Col>
+        <Col className="my-3">
+          <Form.Group controlId="tax">
+            <Form.Label>What was the tax amount on the bill?</Form.Label>
+            <Form.Control
+              className="h-100"
+              type="number"
+              min="0.01"
+              step="0.01"
+              placeholder="Tax"
+              value={billDetails.tax}
+              onChange={handleNumInput}
+              required
+            />
+          </Form.Group>
+        </Col>
+        <Col className="my-3">
+          <Form.Group controlId="tipPercentage">
+            <Form.Label>How much do you want to tip?</Form.Label>
+            <Form.Select
+              className="h-100"
+              placeholder="Tip"
+              value={billDetails.tipPercentage}
+              onChange={handleInput}
+              required
+            >
+              <option disabled>Tip Percentage</option>
+              <option value="0.15">15%</option>
+              <option value="0.18">18%</option>
+              <option value="0.20">20%</option>
+              <option value="Custom">Custom/Gratuity</option>
+            </Form.Select>
+          </Form.Group>
+        </Col>
+        {customTip && (
+          <Col className="my-3">
+            <Form.Group controlId="customTip">
+              <Form.Label>
+                What is the custom tip or gratuity amount?
+              </Form.Label>
+              <Form.Control
+                className="h-100"
+                type="number"
+                min="0.01"
+                step="0.01"
+                placeholder="Tip/Gratuity ($)"
+                value={billDetails.tip}
+                onChange={handleCustomTip}
+                required
+              />
+            </Form.Group>
+          </Col>
         )}
-        <Form.Group className="form-input text-center my-2">
+        <Form.Group className="my-3">
           <Button
             className="green-button"
             id={styles.splitItemBtn}
